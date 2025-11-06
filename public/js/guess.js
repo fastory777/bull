02c866465979fd91cbb74f654013ebc5ac1168c6
fastory777 <fastoryprog@gmail.com>
2025-11-06 09:35:40 +0100

    add validation

--- public/js/guess.js
+++ public/js/guess.js
@@ -2,4 +2,14 @@ document.getElementById("btn_guess").addEventListener("click", function() {
     Guesses++;
     alert(`Guess button clicked (${Guesses}).`);
     // alternativ: ("Guess button clicked (" + Guesses + ").");
+
+    let guess = document.getElementById("guess").value;
+    alert (guess);
+
+    if (!/^\d{4}$/.test(guess)) {
+        alert ("not four digits :-(");
+    }
+    else if (new Set(guess).size !== 4) {
+        alert ("not four unique digits :-(");
+    }
 });
